21c2c868a55fa9a512eab87f4ff4cdc7ad185133
karafra <darius.kralovic1@protonmail.com>
2022-03-23 23:51:06 +0100

    :bricks: Twitter client is now bound to original class

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -6,7 +6,7 @@ public static class DependencyInjection
 {
   public static IServiceCollection AddTwitterClient(this IServiceCollection services)
   {
-    services.AddSingleton<TwitterClient>();
+    services.AddSingleton<TwitterSharp.Client.TwitterClient, TwitterClient>();
     return services;
   }
 }
