a97a4abf92e49e14c049b4d48f3a843e959fd3d5
karafra <darius.kralovic1@protonmail.com>
2022-03-25 21:10:57 +0100

    :memo: Add documentation

--- src/Presentation/Startup.cs
+++ src/Presentation/Startup.cs
@@ -10,6 +10,9 @@ namespace Presentation;
 internal sealed class Startup
 {
 
+  /// <summary>
+  /// Root of configuration
+  /// </summary>
   public IConfigurationRoot Configuration { get; }
 
   public Startup()
@@ -22,6 +25,10 @@ internal sealed class Startup
     Configuration = builder.Build();
   }
 
+  /// <summary>
+  /// Mounts services to service collection
+  /// </summary>
+  /// <param name="services">Service collection to mount services to</param>
   public void ConfigureServices(IServiceCollection services)
   {
     services.AddLogging(config => {
@@ -38,6 +45,10 @@ internal sealed class Startup
     services.AddSwaggerGen();
   }
 
+  /// <summary>
+  /// Configures mounted services on application level
+  /// </summary>
+  /// <param name="app">App on which services are configured</param>
   public void Configure(IApplicationBuilder app)
   {
     app.UseWebSockets(new WebSocketOptions
