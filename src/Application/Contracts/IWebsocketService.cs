006790a2acceda5d96a3cdcd2e2b020edd6590ef
karafra <darius.kralovic1@protonmail.com>
2022-03-25 21:10:43 +0100

    :memo: Add documentation

--- src/Application/Contracts/IWebsocketService.cs
+++ src/Application/Contracts/IWebsocketService.cs
@@ -3,9 +3,24 @@ using System.Net.WebSockets;
 
 namespace Application.Contracts;
 
+/// <summary>
+/// Interface for WebSocketService
+/// </summary>
 public interface IWebSocketService
 {
+  /// <summary>
+  /// Method that should verify connection
+  /// </summary>
+  /// <param name="socket">Senders socket</param>
+  /// <param name="cancellationToken">Cancellation token</param>
+  /// <returns></returns>
   public Task Echo(WebSocket socket, CancellationToken cancellationToken = default);
 
+  /// <summary>
+  /// Should stream tweets to given websocket
+  /// </summary>
+  /// <param name="socket">Socket opened by user</param>
+  /// <param name="cancellationToken">Cancellation token</param>
+  /// <returns></returns>
   public Task StreamTweets(WebSocket socket, CancellationToken cancellationToken = default);
 }
