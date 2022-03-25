928ac651d4136e760d7f431f0d30ea3cd6104b01
karafra <darius.kralovic1@protonmail.com>
2022-03-25 21:10:53 +0100

    :memo: Add documentation

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -2,8 +2,17 @@ using Microsoft.Extensions.DependencyInjection;
 using Infrastructure.Twitter;
 
 namespace Infrastructure;
+
+/// <summary>
+/// DI for Infrastructure layer 
+/// </summary>
 public static class DependencyInjection
 {
+  /// <summary>
+  /// Configures services of infrastructure layer
+  /// </summary>
+  /// <param name="services">Service collection application</param>
+  /// <returns>Configured collection</returns>
   public static IServiceCollection AddTwitterClient(this IServiceCollection services)
   {
     services.AddSingleton<TwitterSharp.Client.TwitterClient, TwitterClient>();
